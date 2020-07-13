f412b14bfc82f32d4b0bbbdf5e554376599e2f2e
Octavio Kidd <Octavio.Kidd@powerplan.com>
2020-07-13 07:24:51 -0400

    Removing id from FileInfo projection DTO

--- src/main/java/org/okidd/entities/FileInfo.java
+++ src/main/java/org/okidd/entities/FileInfo.java
@@ -7,27 +7,20 @@ import java.util.Objects;
  */
 public class FileInfo {
 	
-	private final Long id;
 	private final String name;
 	private final Long version;
 	private final String contentType;
 	
-	public FileInfo(Long id, String name, Long version, String contentType) {
-		this.id = id;
+	public FileInfo(String name, Long version, String contentType) {
 		this.name = name;
 		this.version = version;
 		this.contentType = contentType;
 	}
 	
 	public FileInfo(File file) {
-		this.id = file.getId();
 		this.name = file.getName();
 		this.version = file.getVersion();
 		this.contentType = file.getContentType();
-	}
-	
-	public Long getId() {
-		return id;
 	}
 	
 	public String getName() {
